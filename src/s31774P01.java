8b585c33c7edc8b28f699e8e38c3dba2b259383a
Michał Król <33579056+Wolfie011@users.noreply.github.com>
2024-05-13 22:50:01 +0200

    zmianki

--- src/s31774P01.java
+++ src/s31774P01.java
@@ -6,109 +6,75 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.Scanner;
 
-public class s31774P01 {
+public class s31774P01 implements directoryMethods, edtoryInterface{
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        // 0 - oznacza ze jestesmy w menu podstawowym w ktorym wybieramy modul
-        // 1.. czym glebiej i tak dalej
-        // komendy w zaleznosci jakie wybierzemy bedą zmieniac status czyli nasze połozenie w programie
-
         int statusCode = 0;
 
         Map<Integer, String> Status = new HashMap<>(){{
             put(0, "Main");
             put(10, "Menu edycji");
-            put(20, "Menu sprawdzania");
             put(11, "Dodawanie zadań");
             put(12, "");
-            put(14, "Menu edycji");
+            put(20, "Menu sprawdzania");
+            put(21, "Zadania");
         }};
 
         List<File> folderList = new ArrayList<>() {{
             add(new File("./src/Zadania"));
-            add(new File("./src/Zadania"));
+            add(new File("./src/Studenci"));
         }};
 
         load(folderList);
-
-        Map<String, Command> commandMapMain = new HashMap<>() {{
-            put("stop", new Command("stop", "Kończy cały program", 0));
-            put("edit", new Command("edit", "edytuje", 1));
+        enum CommandKey {
+            STOP,
+            EDITMODE,
+            ADD,
+            BACK,
+            EDIT
+        }
+        Map<CommandKey, Command> commandMapMain = new HashMap<>() {{
+            put(CommandKey.STOP, new Command("stop", "Kończy cały program", -1 ));
+            put(CommandKey.EDITMODE, new Command("edit", "Moduł edycji", 10));
         }};
 
-        Map<String, Command> commandMapEdit = new HashMap<>() {{
-            put("add", new Command("add", "Dodaje nowe zadanie", 0));
-            put("edit", new Command("edit", "edytuje", 1));
+        Map<CommandKey, Command> commandMapEdit = new HashMap<>() {{
+            put(CommandKey.ADD, new Command("add", "Dodaje nowe zadanie", 10, edtoryInterface.runEditor()));
+            put(CommandKey.EDIT, new Command("edit", "edytuje", 10));
+            put(CommandKey.BACK, new Command("backToMain", "Powrót do maina", 0));
         }};
 
-        Map<Integer, Map<String, Command>> commandMap = new HashMap<>(){{
+        Map<Integer, Map<CommandKey, Command>> commandMap = new HashMap<>(){{
             put(0, commandMapMain);
             put(10, commandMapEdit);
         }};
 
-        try {
-            File Zadania = new File("Zadania");
-            Zadania.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
         do {
+            if (statusCode == -1) System.exit(0);
             System.out.println("##########################################################################");
             System.out.println("Aktualna pozycja: " + Status.get(statusCode) + ", Aktualny status: " + statusCode);
-            for(Map.Entry<String, Command> entry : commandMapMain.entrySet()){
+            for(Map.Entry<CommandKey, Command> entry : commandMap.get(statusCode).entrySet()){
                 System.out.println(entry.getKey() + " " + entry.getValue().toString());
             }
             System.out.print("Podaj komende: ");
             String input = scanner.next();
-            if (input.equals("stop")){
-                break;
-            }
-            Command command = commandMap.get(statusCode).get(input);
+
+            Command command = commandMap.get(statusCode).get(CommandKey.valueOf(input));
             if (command != null){
                 statusCode = command.StatusCode;
+                if (command.function != null) command.function.run();
             }else {
                 System.out.println("Nieznana komenda.");
             }
         }while (true);
     }
 
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
-
     public static void load(List<File> files) {
         for (File folder : files) {
-            if (!folder.exists()) {
-                boolean created = folder.mkdirs();
-                if (created) {
-                    System.out.println("Folder created successfully!");
-                } else {
-                    System.out.println("Failed to create folder.");
-                }
-            } else {
-                System.out.println("Folder already exists.");
-            }
+            directoryMethods.createDirectory(folder);
         }
-
-//    public static void add() {
-//        try {
-//            File fZadania = new File("./src/Zadania");
-//            if (fZadania.createNewFile()) {
-//                System.out.println("Tworzenie folderu do zadań.. ");
-//            } else {
-//                System.out.println("Sprawdzanie folderów.. ");
-//            }
-//        } catch (IOException e) {
-//            System.out.println("An error occurred.");
-//            e.printStackTrace();
-//        }
-//    }
     }
-
-
 }
 abstract class Task{
     int taskNum;
@@ -156,7 +122,13 @@ class Command {
     String commandName;
     String commandDescription;
     int StatusCode;
-
+    Runnable function;
+    public Command(String commandName, String commandDescription, int StatusCode, Runnable function) {
+        this.commandDescription = commandDescription;
+        this.commandName = commandName;
+        this.StatusCode = StatusCode;
+        this.function = function;
+    }
     public Command(String commandName, String commandDescription, int StatusCode) {
         this.commandDescription = commandDescription;
         this.commandName = commandName;
@@ -165,5 +137,50 @@ class Command {
     @Override
     public String toString() {
         return " - " + commandDescription + " [ StatusCode: " + StatusCode + " ]";
+    }
+}
+interface edtoryInterface {
+    static Runnable runEditor() {
+        return () -> {
+            Scanner scanner = new Scanner(System.in);
+            String zadanieTresc = "";
+
+            while (true){
+                System.out.print("> ");
+                String input = scanner.nextLine();
+                if (input.equals(">exit")) {
+                    System.out.println("Utworz oraz zapisz? [Y/N]: ");
+                    if(input.equals("Y")){
+                        //TUTAJ ROBIMY ZAPUIS
+                        break;
+                    } else {
+                        break;
+                    }
+                }
+                zadanieTresc += input;
+                zadanieTresc += "\n";
+            }
+            System.out.println(zadanieTresc);
+        };
+    }
+}
+interface directoryMethods{
+    static File createDirectory(File folder){
+        if (!folder.exists()) {
+            boolean created = folder.mkdirs();
+            if (created) {
+                System.out.println("Folder created successfully!");
+            } else {
+                System.out.println("Failed to create folder.");
+            }
+        } else {
+            System.out.println("Folder already exists.");
+        }
+
+        return folder;
+    }
+    static void creator(String name){
+        File nowyKatalogZadania = createDirectory(new File("./src/Zadania/" + name));
+
     }
 }
